930d423362257822cc512bca19276a66620ed5cb
Ingrida <a.ingrida@gmail.com>
2026-09-08 21:54:23 +0300

    Pridėta atsitiktinė žinutė

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ mygtukas.addEventListener("click", () => {
   skaicius = skaicius + 1;
   mygtukas.textContent = "Paspausta: " + skaicius;
 });
+const zinutes = [ 
+  "Šiandien puiki diena kodui", 
+  "Git nieko nepamiršta", 
+  "Commit dažnai, gailėsies rečiau", 
+  "Merge conflict – ne katastrofa",
+];
+  const zinutesMygtukas = document.getElementById("zinutesMygtukas");
+  const zinute = document.getElementById("zinute");
+  zinutesMygtukas.addEventListener("click", () => {
+     const atsitiktinis = Math.floor(Math.random() * zinutes.length);
+     zinute.textContent = zinutes[atsitiktinis];
+    });
