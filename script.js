a8dd806c0386bb09b8f11a81e262eee686535984
Jeanne <zana.kel777@gmail.com>
2026-09-10 18:34:40 +0300

    Patikslinimas

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ const zinutes = [
      const atsitiktinis = Math.floor(Math.random() * zinutes.length);
      zinute.textContent = zinutes[atsitiktinis];
     });
+
+const spalvos = ["#411010", "#10412a", "#102841", "#41103d"];
+let spalvosIndeksas = 0;
+const spalvosMygtukas = document.getElementById("spalvosMygtukas");
+
+spalvosMygtukas.addEventListener("click", () => {
+  spalvosIndeksas = (spalvosIndeksas + 1) % spalvos.length;
+  document.body.style.background = spalvos[spalvosIndeksas];
+});
+
+let taip = 0;
+let ne = 0;
+const taipMygtukas = document.getElementById("taipMygtukas");
+const neMygtukas = document.getElementById("neMygtukas");
+const rezultatas = document.getElementById("rezultatas");
+
+function rodykRezultata() {
+  rezultatas.textContent = "Taip: " + taip + " | Ne: " + ne;
+}
+
+taipMygtukas.addEventListener("click", () => {
+  taip = taip + 1;
+  rodykRezultata();
+});
+
+neMygtukas.addEventListener("click", () => {
+  ne = ne + 1;
+  rodykRezultata();
+});
